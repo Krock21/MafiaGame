e59ce3ed54e828e1344d7b0f520743ca429c42e0
Vladilav Yepifanov <evn1999@yandex.ru>
2019-04-21 21:28:51 +0300

    deleted unused activity

--- app/src/main/java/me/hwproj/mafiagame/MainActivity.java
+++ app/src/main/java/me/hwproj/mafiagame/MainActivity.java
@@ -1,8 +1,11 @@
 package me.hwproj.mafiagame;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.ViewModelProviders;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TestStringHolder h = ViewModelProviders.of(this).get(TestStringHolder.class);
+        Button bAddC = findViewById(R.id.button);
+        Button bReset = findViewById(R.id.button2);
+        TextView text = findViewById(R.id.textView);
+
+        bAddC.setOnClickListener(v -> h.append('c'));
+        bReset.setOnClickListener(v -> h.setText(""));
+        h.getData().observe(this, text::setText);
     }
 }

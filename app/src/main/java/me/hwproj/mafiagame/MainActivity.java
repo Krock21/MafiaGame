ecdee102fa55426c8f4c2af3a2ec438655d3f346
krock21rus <krock21rus@gmail.com>
2019-05-07 00:29:44 +0300

    added sign in function

--- app/src/main/java/me/hwproj/mafiagame/MainActivity.java
+++ app/src/main/java/me/hwproj/mafiagame/MainActivity.java
@@ -2,6 +2,7 @@ package me.hwproj.mafiagame;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -11,8 +12,23 @@ import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.lifecycle.ViewModelProviders;
+
+import com.google.android.gms.auth.api.Auth;
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.auth.api.signin.GoogleSignInResult;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.games.Games;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+
 import me.hwproj.mafiagame.gameflow.Client;
 import me.hwproj.mafiagame.gameflow.Server;
 import me.hwproj.mafiagame.gameflow.ServerGameData;
@@ -24,6 +40,8 @@ import me.hwproj.mafiagame.impltest.TestPhaseGameState;
 import me.hwproj.mafiagame.impltest.TestPhaseServer;
 
 public class MainActivity extends AppCompatActivity {
+    private static final int RC_SIGN_IN = 9000;
+    private GoogleSignInAccount googleSignInAccount;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,11 +51,11 @@ public class MainActivity extends AppCompatActivity {
         TestStringHolder h = ViewModelProviders.of(this).get(TestStringHolder.class);
         Button bAddC = findViewById(R.id.button);
         Button bReset = findViewById(R.id.button2);
+        Button bSignIn = findViewById(R.id.signin);
         TextView text = findViewById(R.id.textView);
 
         bAddC.setOnClickListener(v -> h.append('c'));
         bReset.setOnClickListener(v -> h.setText(""));
-
 
 
         Thread threadPrinter = new Thread(() -> {
@@ -55,6 +73,78 @@ public class MainActivity extends AppCompatActivity {
 
         Button start = findViewById(R.id.startTest);
         start.setOnClickListener(this::startTestPhase);
+
+        bSignIn.setOnClickListener(v -> {
+            startSignInIntent();
+        });
+
+        //signInSilently();
+    }
+
+    private void signInSilently() {
+        GoogleSignInOptions signInOptions = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(MainActivity.this.getResources().getString(R.string.server_client_id))
+                .build();
+        GoogleSignInAccount account = GoogleSignIn.getLastSignedInAccount(this);
+        if (GoogleSignIn.hasPermissions(account, signInOptions.getScopeArray())) {
+            // Already signed in.
+            // The signed in account is stored in the 'account' variable.
+            googleSignInAccount = account;
+        } else {
+            // Haven't been signed-in before. Try the silent sign-in first.
+            GoogleSignInClient signInClient = GoogleSignIn.getClient(this, signInOptions);
+            signInClient
+                    .silentSignIn()
+                    .addOnCompleteListener(
+                            this,
+                            new OnCompleteListener<GoogleSignInAccount>() {
+                                @Override
+                                public void onComplete(@NonNull Task<GoogleSignInAccount> task) {
+                                    if (task.isSuccessful()) {
+                                        // The signed in account is stored in the task's result.
+                                        googleSignInAccount = task.getResult();
+                                    } else {
+                                        // Player will need to sign-in explicitly using via UI.
+                                        // See [sign-in best practices](http://developers.google.com/games/services/checklist) for guidance on how and when to implement Interactive Sign-in,
+                                        // and [Performing Interactive Sign-in](http://developers.google.com/games/services/android/signin#performing_interactive_sign-in) for details on how to implement
+                                        // Interactive Sign-in.
+                                        // signing in
+                                        startSignInIntent();
+                                    }
+                                }
+                            });
+        }
+    }
+
+    private void startSignInIntent() {
+        GoogleSignInOptions signInOptions = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(MainActivity.this.getResources().getString(R.string.server_client_id))
+                .build();
+        GoogleSignInClient signInClient = GoogleSignIn.getClient(this,
+                signInOptions);
+        Intent intent = signInClient.getSignInIntent();
+        startActivityForResult(intent, RC_SIGN_IN);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == RC_SIGN_IN) {
+            GoogleSignInResult result = Auth.GoogleSignInApi.getSignInResultFromIntent(data);
+            if (result.isSuccess()) {
+                // The signed in account is stored in the result.
+                googleSignInAccount = result.getSignInAccount();
+                new AlertDialog.Builder(this).setMessage("SigningIn SUCCESS")
+                        .setNeutralButton(android.R.string.ok, null).show();
+            } else {
+                String message = result.getStatus().toString();
+                if (message == null || message.isEmpty()) {
+                    message = getString(R.string.signin_other_error);
+                }
+                new AlertDialog.Builder(this).setMessage(message)
+                        .setNeutralButton(android.R.string.ok, null).show();
+            }
+        }
     }
 
     private void startTestPhase(View v) {
